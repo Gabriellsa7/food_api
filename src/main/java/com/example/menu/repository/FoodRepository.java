9abcf0a16519ab1f1c3bc2906f219bba73b8107f
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-18 13:37:18 -0300

    change to UUID the params

--- src/main/java/com/example/menu/repository/FoodRepository.java
+++ src/main/java/com/example/menu/repository/FoodRepository.java
@@ -3,5 +3,7 @@ package com.example.menu.repository;
 import com.example.menu.model.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FoodRepository extends JpaRepository<Food, Long> {
+import java.util.UUID;
+
+public interface FoodRepository extends JpaRepository<Food, UUID> {
 }
