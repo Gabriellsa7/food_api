01a9857768ae195ea8c614febd0ca74e91009a00
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-18 09:53:50 -0300

    created FoodResponseDTO Record

---
+++ src/main/java/com/example/menu/record/FoodResponseDTO.java
@@ -0,0 +1,9 @@
+package com.example.menu.record;
+
+import com.example.menu.model.Food;
+
+public record FoodResponseDTO(Long id, String title, String image, Integer price) {
+    public FoodResponseDTO(Food food) {
+        this(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
+    }
+}
