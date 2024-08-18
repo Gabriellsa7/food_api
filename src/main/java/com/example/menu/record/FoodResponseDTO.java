3a3f1fd32cbb39fea6224a0265e8f3a652d5bdcd
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-18 13:37:33 -0300

    change id to UUID

--- src/main/java/com/example/menu/record/FoodResponseDTO.java
+++ src/main/java/com/example/menu/record/FoodResponseDTO.java
@@ -2,7 +2,9 @@ package com.example.menu.record;
 
 import com.example.menu.model.Food;
 
-public record FoodResponseDTO(Long id, String title, String image, Integer price) {
+import java.util.UUID;
+
+public record FoodResponseDTO(UUID id, String title, String image, Integer price) {
     public FoodResponseDTO(Food food) {
         this(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
     }
